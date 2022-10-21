926db2daa13a96b249dab5e123845c2012c47765
Alexandre Junior <alexandrejuniordev@gmail.com>
2022-10-21 15:26:40 -0300

    configura formulário com react hook form com campo de pesquisa de transações

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -2,31 +2,12 @@ import React, { useContext } from 'react'
 
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
 
-import { TransactionsContext } from '../../contexts/TransactionsContext'
+import { useSummary } from '../../hooks/useSummary'
 import { priceFormatter } from '../../utils/formatter'
 import { SummaryCard, SummaryContainer } from './styles'
 
 export const Summary = () => {
-  const { transactions } = useContext(TransactionsContext)
-
-  const summary = transactions.reduce(
-    (accumulator, transaction) => {
-      if (transaction.type === 'income') {
-        accumulator.income += transaction.price
-        accumulator.total += transaction.price
-      } else {
-        accumulator.outcome += transaction.price
-        accumulator.total -= transaction.price
-      }
-
-      return accumulator
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  )
+  const summary = useSummary()
 
   return (
     <SummaryContainer>
--- src/pages/ Transactions/components/SearchForm/index.tsx
+++ src/pages/ Transactions/components/SearchForm/index.tsx
@@ -1,13 +1,43 @@
+import { useForm } from 'react-hook-form'
+
+import { zodResolver } from '@hookform/resolvers/zod'
 import { MagnifyingGlass } from 'phosphor-react'
+import * as z from 'zod'
 
 import { SearchFormContainer } from './styles'
 
-export const SearchForm = () => {
-  return (
-    <SearchFormContainer>
-      <input type="text" placeholder="Busque por transações" />
+const searchFormSchema = z.object({
+  query: z.string(),
+})
 
-      <button type="submit">
+type SearchFormInputs = z.infer<typeof searchFormSchema>
+
+export const SearchForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: {
+      isSubmitting /* propriedade que retorna quando o formulário está sendo enviado */,
+    },
+  } = useForm<SearchFormInputs>({
+    resolver: zodResolver(searchFormSchema),
+  })
+
+  const handleSearchTransactions = async (data: SearchFormInputs) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+
+    await console.log(data)
+  }
+
+  return (
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input
+        type="text"
+        placeholder="Busque por transações"
+        {...register('query')}
+      />
+
+      <button type="submit" disabled={isSubmitting}>
         <MagnifyingGlass size={20} />
         Buscar
       </button>
