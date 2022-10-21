48458f186d7e4c09be5af3eb1a8d6316907afadf
Alexandre Junior <alexandrejuniordev@gmail.com>
2022-10-21 12:35:29 -0300

    adiciona biblioteca radix para fazer o modal e já cria o corpo inicial do modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import * as Dialog from '@radix-ui/react-dialog'
+
 import logoImg from '../../assets/logo.svg'
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
 
@@ -7,7 +9,21 @@ export const Header = () => {
       <HeaderContent>
         <img src={logoImg} alt="" />
 
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          {/* portal para renderizar o componente em outro lugar */}
+          <Dialog.Portal>
+            <Dialog.Overlay /> {/* fundo preto do modal */}
+            <Dialog.Content>
+              <Dialog.Title>Nova transação</Dialog.Title>
+
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
