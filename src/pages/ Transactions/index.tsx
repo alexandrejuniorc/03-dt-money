0179691b80a2c26523a6e09aaa137f969280ccdb
Alexandre Junior <alexandrejuniordev@gmail.com>
2022-10-21 15:06:10 -0300

    cria formatador de data e preço

--- src/pages/ Transactions/index.tsx
+++ src/pages/ Transactions/index.tsx
@@ -3,6 +3,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
 import { TransactionsContext } from '../../contexts/TransactionsContext'
+import { dateFormatter, priceFormatter } from '../../utils/formatter'
 import { SearchForm } from './components/SearchForm'
 import {
   PriceHighLight,
@@ -28,11 +29,14 @@ export const Transactions = () => {
                 <td width="50%">{transaction.description}</td>
                 <td>
                   <PriceHighLight variant={transaction.type}>
-                    {transaction.price}
+                    {transaction.type === 'outcome' && '- '}
+                    {priceFormatter.format(transaction.price)}
                   </PriceHighLight>
                 </td>
                 <td>{transaction.category}</td>
-                <td>{transaction.created_at}</td>
+                <td>
+                  {dateFormatter.format(new Date(transaction.created_at))}
+                </td>
               </tr>
             ))}
           </tbody>
